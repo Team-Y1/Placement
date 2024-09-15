5802baf3c30ffddd17e387a34a0e2078a85eb86e
Saiganesh Raju Gottam <121511326+gsaiganesh283@users.noreply.github.com>
2024-09-15 12:36:12 +0530

    list example code is executed

---
+++ DSA/Arrays and Strings/List.py
@@ -0,0 +1,20 @@
+arr = [1,2,3,4,5]
+
+print(arr[0]) # printing value at index '0'
+
+print(arr[:-1]) # printing all the elements in array
+
+arr.append(6) # adding an element into the array
+
+print(arr)
+
+arr.insert(1,12) # inserting the element 12 at postion '1'
+
+print(arr)
+
+arr.remove(5) # remove first occurence of element '5'
+
+print(arr)
+
+print(arr[1:4]) # slicing printing the elements from index '1' to '3'
+
